7de2be79045d843c6c7968ef2b81e8e668f8d772
Crowdbotics <team@crowdbotics.com>
2024-02-23 18:52:31 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "portugueselanguaget-46614",
+  url: "https://portugueselanguaget-46614.botics.co",
+  api: "https://portugueselanguaget-46614.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
